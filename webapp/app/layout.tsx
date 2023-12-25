609c1aa4a54fdc81f91fdd3b37d472502886189a
jonasViehweger <jonas.viehweger@rwth-aachen.de>
2023-12-25 17:12:06 +0100

    Added logo

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -16,8 +16,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen w-screen bg-gradient-to-r from-indigo-50 to-rose-100 flex flex-col">
-        <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-40 mt-5 rounded-full mx-auto text-center shadow-md">
-          <Link href="/adm0">Countries</Link>
+        <div className="flex flex-row items-center mt-5">
+          <p className="font-sans font-light text-4xl absolute left-10 text-slate-700">
+            <Link href="/">shapefinder</Link>
+          </p>
+          <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-40 mt-5 rounded-full mx-auto text-center shadow-md">
+            <Link href="/adm0">Countries</Link>
+          </div>
         </div>
         <section className="grow">{children}</section>
         <div className="flex flex-row justify-center items-center h-20">
