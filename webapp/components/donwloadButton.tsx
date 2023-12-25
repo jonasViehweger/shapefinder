ced39a9b8aaa6e36eef9c27ccc4eb31cae6db54d
jonasViehweger <jonas.viehweger@rwth-aachen.de>
2023-12-25 21:46:24 +0100

    More meaningful name of download

--- webapp/components/donwloadButton.tsx
+++ webapp/components/donwloadButton.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { usePathname } from "next/navigation";
 
 export default function DownloadButton({ message }: { message: string }) {
   const file = new Blob([message], { type: "text/plain" });
@@ -16,7 +17,7 @@ export default function DownloadButton({ message }: { message: string }) {
       className="bg-blue-500 text-white shadow-md rounded-full h-14 w-14 text-lg hover:bg-blue-600"
     >
       <a
-        download="border.geojson"
+        download={`${usePathname().slice(1).toUpperCase()}.geojson`}
         target="_blank"
         rel="noreferrer"
         href={URL.createObjectURL(file)}
